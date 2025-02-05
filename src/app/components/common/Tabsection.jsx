ae6a3b680c6e384f1f78b8bffafb329db5d8644d
happy61300 <happyjaat61300@gmail.com>
2025-02-05 11:14:55 +0530

    make sick care

--- src/app/components/common/Tabsection.jsx
+++ src/app/components/common/Tabsection.jsx
@@ -34,7 +34,61 @@ function Tabsection() {
       <div className="flex lg:flex-nowrap flex-wrap">
         <div className="lg:w-2/5">
           <TabLeft
-            img={"/assest/img/pre-care.png"}
+            img={"/assest/img/sick-care.png"}
+            title={"Sick Patient Care"}
+            subTitle={
+              "Delivering exceptional care for sick patients, combining the virtues of efficiency and expertise"
+            }
+            dis={
+              "At Nebula Vets, we are fully equipped with advanced diagnostic tools and a wide range of treatment options to address any health issue your pet may be facing. Our commitment to comprehensive care means that we have the expertise and resources to provide accurate diagnoses and effective treatments."
+            }
+            btnName={" BOOK NOW"}
+          />
+        </div>
+        <div className="lg:w-3/5 bg-[#D7D9DD80] pl-10 pt-10 md:pt-20 md:pl-20 pr-10 ">
+          <div>
+            {sickData.map((item, index) => (
+              <TabRight
+                key={index}
+                title={item.head}
+                dis={item.dis}
+                btnName={item.btnName}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex lg:flex-nowrap flex-wrap">
+        <div className="lg:w-2/5">
+          <TabLeft
+            img={"/assest/img/sick-care.png"}
+            title={"Sick Patient Care"}
+            subTitle={
+              "Delivering exceptional care for sick patients, combining the virtues of efficiency and expertise"
+            }
+            dis={
+              "At Nebula Vets, we are fully equipped with advanced diagnostic tools and a wide range of treatment options to address any health issue your pet may be facing. Our commitment to comprehensive care means that we have the expertise and resources to provide accurate diagnoses and effective treatments."
+            }
+            btnName={" BOOK NOW"}
+          />
+        </div>
+        <div className="lg:w-3/5 bg-[#D7D9DD80] pl-10 pt-10 md:pt-20 md:pl-20 pr-10 ">
+          <div>
+            {sickData.map((item, index) => (
+              <TabRight
+                key={index}
+                title={item.head}
+                dis={item.dis}
+                btnName={item.btnName}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex lg:flex-nowrap flex-wrap">
+        <div className="lg:w-2/5">
+          <TabLeft
+            img={"/assest/img/sick-care.png"}
             title={"Sick Patient Care"}
             subTitle={
               "Delivering exceptional care for sick patients, combining the virtues of efficiency and expertise"
--- src/app/components/helper/Helper.jsx
+++ src/app/components/helper/Helper.jsx
@@ -52,23 +52,23 @@ export const precareData = [
 ];
 export const sickData = [
   {
-    head: "Animal Health Check",
-    dis: "Our practice offers the convenience of same or next-day appointments to ensure your furry companion receives prompt diagnostic evaluation and treatment. Our dedicated team prioritizes swift action, allowing us to quickly identify the issue at hand and provide the necessary care your pet requires.",
+    head: "Urgent & Emergency Care",
+    dis: "Whether your pet is sick or injured, we’re here to help get them the help they need. This includes telemedicine veterinary consults if you are unsure, and overnight care for 24-hour treatment.",
     btnName: "LEARN MORE",
   },
   {
-    head: "Vaccinations",
-    dis: "Vaccinations are a key component of your pet’s health and essential to prevent life-threatening diseases. Nebula vets offer's all the core and non-core vaccines your pet might need.",
+    head: "Telemedicine consults",
+    dis: "Nebula vets offers a fast telemedicine service, to help your pet without leaving the home",
     btnName: "LEARN MORE",
   },
   {
-    head: "Dentals",
-    dis: "Dental disease and smelly breath is common, and regular cleanings and exams can save your pet a lot of pain and disease later in life. Our advanced state-of-the-art dental suite is equipped for dental cleanings (scale and polish), extractions and any other care your pet may need.",
+    head: "Diagnostics",
+    dis: "Nebula vets has invested in the latest technology, equipped for all the diagnostic tests your pet needs to stay healthy, and to treat those that get sick. We’re able to run tests quickly in-house in situations where a rapid diagnosis is required and offer artificialintelligence alongside veterinary experts.",
     btnName: "LEARN MORE",
   },
   {
-    head: "Dentals",
-    dis: "Dental disease and smelly breath is common, and regular cleanings and exams can save your pet a lot of pain and disease later in life. Our advanced state-of-the-art dental suite is equipped for dental cleanings (scale and polish), extractions and any other care your pet may need.",
+    head: "Pharmacy",
+    dis: "Our highly stocked on-site pharmacy inlcudes a variety of prescription medications and pet health products for your pet. If a particular medication is unavailable, our compounding pharmacy can have it delivered to you within 24 hours.",
     btnName: "LEARN MORE",
   },
 ];
